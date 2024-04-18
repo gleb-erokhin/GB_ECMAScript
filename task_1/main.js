48db45b943cbc1d89889b0810f121d009c94e457
gleb-erokhin <gleb.erokhin@gmail.com>
2024-04-18 22:13:05 +0300

    Задача 1 готова и стилизована

--- task_1/main.js
+++ task_1/main.js
@@ -17,3 +17,19 @@ const Book = {
 // Метод displayInfo() - выводит информацию о книге (название, автор и количество страниц).
 
 Book.displayInfo();
+
+// функция для создания элементов HTML
+function createElement(tag, className, showText="") {
+    const item = document.createElement(tag);
+    item.classList.add(className)
+    item.textContent = showText;
+    return item;
+}
+
+const mainTag = createElement('div', 'main');
+const header = createElement('h1', 'text', 'Управление библиотекой книг');
+const paragraf = createElement('p', 'info', `Название: ${Book.title}, Автор: ${Book.author}, Количество страниц: ${Book.pages}`)
+
+// Добавляем элементы на страницу
+document.body.append(mainTag);
+mainTag.append(header, paragraf);
