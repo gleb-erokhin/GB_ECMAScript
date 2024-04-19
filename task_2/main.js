83db4086a7425a326f498847b81c7a5aaaf088bf
gleb-erokhin <gleb.erokhin@gmail.com>
2024-04-19 07:09:23 +0300

    задание 2 выполнено

--- task_2/main.js
+++ task_2/main.js
@@ -1,12 +1,14 @@
 // Задание 2: ""Управление списком студентов""
 // Реализуйте класс Student, представляющий студента, со следующими свойствами и методами:
 
-const Student = {
-    names: this.name,
-    age: this.age,
-    grade: this.grade,
+class Student {
+    constructor (name, age, grade) {
+        this.name = name,
+        this.age = age,
+        this.grade =grade
+    }
     displayInfo() {
-        console.log(`название: ${this.name}, Автор: ${this.age}, количество страниц: ${this.grade}`);;
+        return `имя студента: ${this.name}, возраст студента: ${this.age}, класс, в котором учится студент: ${this.grade}`;
     }
 }
 // Свойство name (имя) - строка, имя студента.
@@ -29,3 +31,28 @@ student2.displayInfo();
 // Name: Jane Doe
 // Age: 17
 // Grade: 11th grade"
+
+const student3 = new Student("Ann Ford", 18, "19th grade");
+student3.displayInfo();
+
+// визуальное оформление
+
+// функция для создания элементов HTML
+function createElement(tag, className, insertText="") {
+    const item = document.createElement(tag);
+    item.classList.add(className)
+    item.textContent = insertText;
+    return item;
+}
+
+const mainTag = createElement('div', 'main');
+const header = createElement('h1', 'text', 'Управление списком студентов');
+const divBlock = createElement('div', 'block')
+const paragraf1 = createElement('p', 'info', student1.displayInfo())
+const paragraf2 = createElement('p', 'info', student2.displayInfo())
+const paragraf3 = createElement('p', 'info', student3.displayInfo())
+
+// Добавляем элементы на страницу
+document.body.append(mainTag);
+mainTag.append(header, divBlock);
+divBlock.append(paragraf1, paragraf2, paragraf3);
